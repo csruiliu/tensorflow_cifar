801f2ef8fb1474c40ab1841283b7cce074daec07
csruiliu <rui.liu@proton.me>
2021-01-05 11:25:40 -0600

    add resnext

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ All the models are tested on TensorFlow 1.15 and should be compatible with TF 1.
 
 ## AlexNet ##
 
-The implementation of AlexNet follows the architecture proposed in the [paper](https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf). To fit the CIFAR-10, I modify the filters and kernel size of conv layer, for example, the filters and kernel of the first conv layer is changed to 64 and 3.
+The implementation of AlexNet follows the architecture proposed in the [paper](https://proceedings.neurips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf). To fit the CIFAR-10, I modify the filters and kernel size of conv layer, for example, the filters and kernel of the first conv layer is changed to 64 and 3. Also, the current hardware offering is powerful enough, so the original two GPUs version's AlexNet is usually implemented in a unified fashion. 
 
 ## DenseNet ##
 
 The implementations of DenseNet [121, 169, 201, 264] follow the architecture proposed in the [paper](https://arxiv.org/pdf/1608.06993.pdf). To fit the CIFAR-10, I slightly modify the kernel size of the front convolutional layer from 7x7 to 3x3, and omit the first 3x3 max pooling layer. 
 
-## GoogLeNet ## 
+## Inception ## 
 
-The implementation of GoogLeNet follows the architecture presented in the [paper](https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/43022.pdf). I remove the front 7x7 conv layer and 3x3 max pooling layer to fit CIFAR-10. 
+The implementation of Inception (aka GoogLeNet, Inception-V1) follows the architecture presented in the [paper](https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/43022.pdf). I remove the front 7x7 conv layer and 3x3 max pooling layer to fit CIFAR-10. 
 
 ## LeNet ## 
 
@@ -29,6 +29,11 @@ The implementations of MobileNetV2 basically follow the architecture proposed in
 ## ResNet ##
 
 The implementations of ResNet [18, 34, 50, 101, 152] follow the proposed architecture of the original [paper](https://arxiv.org/abs/1512.03385). To fit the CIFAR-10, I slightly modify the kernel size of `conv_1` from 7x7 to 3x3 and omit the first 3x3 max pooling layer. 
+
+
+## ResNeXt ##
+
+The implementations of ResNeXt follow the proposed architecture of the original [paper](https://arxiv.org/pdf/1611.05431.pdf). Specifically, I follow the original implemention of ResNeXt on CIFAR-10, ResNeXt-29.  
 
 ## VGG ##
 
---
+++ models/resnext.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+
+
+class ResNeXt:
+    def __init__(self, cardinality, bottleneck_width, num_classes):
+        self.card = cardinality
+        self.width = bottleneck_width
+        self.output_classes = num_classes
+
+    def res_block(self, block_input, down_sample=False, scope='block'):
+        expansion = 2
+        group_width = self.card * self.width
+
+        with tf.variable_scope(scope):
+            if down_sample:
+                strides = 2
+                shortcut = tf.keras.layers.Conv2D(filters=expansion*group_width, kernel_size=1,
+                                                  strides=2, use_bias=False)(block_input)
+                shortcut = tf.layers.batch_normalization(shortcut, training=True)
+            else:
+                strides = 1
+                shortcut = block_input
+
+            x = tf.keras.layers.Conv2D(filters=group_width, kernel_size=1, use_bias=False)(block_input)
+            x = tf.layers.batch_normalization(x, training=True)
+            x = tf.keras.activations.relu(x)
+
+            x = tf.keras.layers.Conv2D(filters=group_width, kernel_size=3, strides=strides,
+                                       padding='same', groups=self.card, use_bias=False)(x)
+            x = tf.layers.batch_normalization(x, training=True)
+            x = tf.keras.activations.relu(x)
+
+            x = tf.keras.layers.Conv2D(filters=expansion*group_width, kernel_size=1, use_bias=False)(x)
+            x = tf.layers.batch_normalization(x, training=True)
+
+            if shortcut.shape[-1] != x.shape[-1]:
+                shortcut = tf.keras.layers.Conv2D(expansion*group_width, kernel_size=1,
+                                                  strides=1, use_bias=False)(shortcut)
+                shortcut = tf.layers.batch_normalization(shortcut, training=True)
+
+            layer = tf.keras.activations.relu(x + shortcut)
+
+        return layer
+
+    def fc_layer(self, layer_input, scope='fc'):
+        with tf.variable_scope(scope):
+            layer = tf.keras.layers.Flatten()(layer_input)
+            layer = tf.keras.layers.Dense(units=self.output_classes)(layer)
+
+        return layer
+
+    def build(self, model_input):
+        with tf.variable_scope('conv_1'):
+            x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, use_bias=False)(model_input)
+            x = tf.layers.batch_normalization(x, training=True)
+            x = tf.keras.activations.relu(x)
+
+        # ======== Stage 1 ======== #
+        x = self.res_block(x, scope='blk_1_1')
+        x = self.res_block(x, scope='blk_1_2')
+        x = self.res_block(x, scope='blk_1_3')
+
+        # Increase bottleneck_width by 2 after each stage.
+        self.width = self.width * 2
+
+        # ======== Stage 2 ======== #
+        x = self.res_block(x, down_sample=True, scope='blk_2_1')
+        x = self.res_block(x, scope='blk_2_2')
+        x = self.res_block(x, scope='blk_2_3')
+
+        # Increase bottleneck_width by 2 after each stage.
+        self.width = self.width * 2
+
+        # ======== Stage 3 ======== #
+        x = self.res_block(x, down_sample=True, scope='blk_3_1')
+        x = self.res_block(x, scope='blk_3_2')
+        x = self.res_block(x, scope='blk_3_3')
+
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+
+        model = self.fc_layer(x)
+
+        return model
