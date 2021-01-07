7fa1bbd248caed77cd8d60b92eb05d91ef95b81a
csruiliu <rui.liu@proton.me>
2021-01-06 22:11:45 -0600

    add squeezenet

---
+++ models/squeezenet.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+
+
+class SqueezeNet:
+    def __init__(self, num_classes=10):
+        self.output_classes = num_classes
+
+    @staticmethod
+    def fire_block(block_input, filters_s1x1, filters_e1x1, filters_e3x3, scope='fire_blk'):
+        with tf.variable_scope(scope):
+            x = tf.keras.layers.Conv2D(filters=filters_s1x1, kernel_size=1, padding='same')(block_input)
+            squeeze_x = tf.keras.activations.relu(x)
+            expand_x_1 = tf.keras.layers.Conv2D(filters=filters_e1x1, kernel_size=1, padding='same')(squeeze_x)
+            expand_x_3 = tf.keras.layers.Conv2D(filters=filters_e3x3, kernel_size=3, padding='same')(squeeze_x)
+            layer = tf.concat([expand_x_1, expand_x_3], axis=3)
+
+            return layer
+
+    def fc_layer(self, layer_input, scope='fc'):
+        with tf.variable_scope(scope):
+            layer = tf.keras.layers.Flatten()(layer_input)
+            layer = tf.keras.layers.Dense(units=self.output_classes)(layer)
+
+        return layer
+
+    def build(self, model_input):
+        x = tf.keras.layers.Conv2D(filters=96, kernel_size=3, strides=2, padding='same')(model_input)
+        x = tf.keras.activations.relu(x)
+        x = tf.keras.layers.MaxPool2D(pool_size=2)(x)
+        x = self.fire_block(x, filters_s1x1=16, filters_e1x1=64, filters_e3x3=64, scope='fire_blk_2')
+        x = self.fire_block(x, filters_s1x1=16, filters_e1x1=64, filters_e3x3=64, scope='fire_blk_3')
+        x = self.fire_block(x, filters_s1x1=32, filters_e1x1=128, filters_e3x3=128, scope='fire_blk_4')
+        x = tf.keras.layers.MaxPool2D(pool_size=2)(x)
+        x = self.fire_block(x, filters_s1x1=32, filters_e1x1=128, filters_e3x3=128, scope='fire_blk_5')
+        x = self.fire_block(x, filters_s1x1=48, filters_e1x1=192, filters_e3x3=192, scope='fire_blk_6')
+        x = self.fire_block(x, filters_s1x1=48, filters_e1x1=192, filters_e3x3=192, scope='fire_blk_7')
+        x = self.fire_block(x, filters_s1x1=64, filters_e1x1=256, filters_e3x3=256, scope='fire_blk_7')
+        x = tf.keras.layers.MaxPool2D(pool_size=2)(x)
+        x = self.fire_block(x, filters_s1x1=64, filters_e1x1=256, filters_e3x3=256, scope='fire_blk_7')
+        x = tf.keras.layers.Dropout(rate=0.5)(x)
+        x = tf.keras.layers.Conv2D(filters=1000, kernel_size=1, padding='same')(x)
+        x = tf.keras.activations.relu(x)
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+
+        model = self.fc_layer(x, scope='fc')
+
+        return model
--- perf_demo.py
+++ perf_demo.py
@@ -12,6 +12,7 @@ from models.inception import Inception
 from models.alexnet import AlexNet
 from models.resnext import ResNeXt
 from models.xception import Xception
+from models.squeezenet import SqueezeNet
 
 from tools.dataset_loader import load_cifar10_keras
 from tools.model_tools import train_model
@@ -48,7 +49,8 @@ if __name__ == "__main__":
     # model = Inception(num_classes=10)
     # model = AlexNet(num_classes=10)
     # model = ResNeXt(cardinality=8, bottleneck_width=64, num_classes=10)
-    model = Xception(num_classes=10)
+    # model = Xception(num_classes=10)
+    model = SqueezeNet(num_classes=10)
 
     feature_ph = tf.placeholder(tf.float32, [None, 32, 32, 3])
     label_ph = tf.placeholder(tf.int32, [None, 10])
