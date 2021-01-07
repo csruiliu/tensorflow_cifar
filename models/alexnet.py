ad526bf6d5d4661029e22078449f5e23fbc3973f
csruiliu <rui.liu@proton.me>
2021-01-07 15:22:21 -0600

    revise alexnet

--- models/alexnet.py
+++ models/alexnet.py
@@ -14,25 +14,25 @@ class AlexNet:
 
     def build(self, model_input):
         with tf.variable_scope('conv_1'):
-            x = tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=2, padding='same')(model_input)
+            x = tf.keras.layers.Conv2D(filters=96, kernel_size=3, strides=2, padding='same')(model_input)
             x = tf.keras.activations.relu(x)
             x = tf.keras.layers.MaxPool2D(pool_size=2)(x)
 
         with tf.variable_scope('conv_2'):
-            x = tf.keras.layers.Conv2D(filters=192, kernel_size=3, strides=1, padding='same')(model_input)
+            x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(x)
             x = tf.keras.activations.relu(x)
             x = tf.keras.layers.MaxPool2D(pool_size=2)(x)
 
         with tf.variable_scope('conv_3'):
-            x = tf.keras.layers.Conv2D(filters=384, kernel_size=3, strides=1, padding='same')(model_input)
+            x = tf.keras.layers.Conv2D(filters=384, kernel_size=3, strides=1, padding='same')(x)
             x = tf.keras.activations.relu(x)
 
         with tf.variable_scope('conv_4'):
-            x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(model_input)
+            x = tf.keras.layers.Conv2D(filters=384, kernel_size=3, strides=1, padding='same')(x)
             x = tf.keras.activations.relu(x)
 
         with tf.variable_scope('conv_5'):
-            x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(model_input)
+            x = tf.keras.layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='same')(x)
             x = tf.keras.activations.relu(x)
             x = tf.keras.layers.MaxPool2D(pool_size=2)(x)
 
